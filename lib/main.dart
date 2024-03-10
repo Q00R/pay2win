4430cceb427fbe689adbdb60fde856bc125ff3d7
Q00R <ripbaelol@gmail.com>
2024-03-10 03:29:02 +0200

    added the datepicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 21, 129, 183),
-        body: Center(
-          child: (Text('Hello World!')),
-        ),
+        body: GradientContainer()
       ),
     ),
   );
 }
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(ctx) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepOrange,
+                Colors.greenAccent,
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        );
+      }
+      }
